d9ba3257d7debf7e98c16c1eec5baec884ec885a
meirgoldshtein <m0548491057@gmail.com>
2024-02-12 13:46:14 +0200

    Add
    Reas the csv file given in argv.
    Add ech customer if he not in the list
    If the customer exist updat hes dets
    Sort and print the list

---
+++ server.py
@@ -0,0 +1,40 @@
+import csv
+import sys
+import os
+import socket
+from customer import Customer
+
+if len(sys.argv) < 2 :
+    print("Error: missing csv file name !")
+
+
+csv_file = (sys.argv[1])
+if not os.path.exists(csv_file):
+    with open(csv_file, 'w'):
+        pass
+
+
+customers = []
+with open(csv_file, 'r') as fd: 
+    for line in fd.readlines():
+        fields = line.split(",")
+        index = -1
+        id = int(fields[2])
+        for i, customer in enumerate(customers):
+            if customer.id == id:
+                index = i 
+                break
+        if index >= 0:
+            customers[index].add_debt(int(fields[4]))
+
+        else:
+            customer = Customer(*fields)
+            customers.append(customer)
+
+customers.sort(key=lambda customer: customer.debt)
+for customer in customers:    
+    print(customers)
+
+        
+
+
