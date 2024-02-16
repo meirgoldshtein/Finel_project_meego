f2694e94690a59a6fc30e4ab71563dcc14a9b0d3
meirgoldshtein <m0548491057@gmail.com>
2024-02-16 02:40:43 +0200

    Adding a data structure file
    In this file there will be several types of binary search trees, according to a variety of parameters such as ID, name, debt
    
    A binary search tree class sorted by ID was written

---
+++ bst cusomers.py
@@ -0,0 +1,52 @@
+
+class Node:
+    def __init__(self, first, last, id ,phon ,debt ,date) -> None:
+        self._first = first
+        self._last = last
+        self._id = id
+        self._phon = phon
+        self._debt = int(debt)
+        self._date = date
+        self.right = None
+        self.left = None
+        
+
+
+
+class Id_tree:
+    
+    def __init__(self) -> None:        
+        self.root = None
+
+
+    def add_node(self, customer):
+        if self.root is None:
+            self.root = customer
+            return
+        
+        temp = self.root
+        while True:
+            if customer.id > temp.id:
+                
+                if temp.right is None:
+                    temp.right = customer
+                    break
+                else:
+                    temp = temp.right
+            
+            elif customer.id <= temp.id:
+                
+                if temp.left is None:
+                    temp.left = customer
+                    break
+                else:
+                    temp = temp.left
+
+
+
+
+
+
+
+
+        
