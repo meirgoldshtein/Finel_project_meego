20fb954b73f9111a9b150c9bb698f9cec9f7d6aa
meirgoldshtein <m0548491057@gmail.com>
2024-02-18 22:19:06 +0200

    Added binary trees by last name and by debt
    Unnecessary consumer duplicate check lines have also been deleted

--- bst.py
+++ bst.py
@@ -1,4 +1,4 @@
-
+# A node object for a binary tree that contains a pointer to a consumer object, and its two children
 class Node:
     def __init__(self, customer) -> None:
         self.customer = customer
@@ -6,16 +6,12 @@ class Node:
         self.left = None
   
         
-
-
-
+#  Binary tree sorted by first name
 class Fname_tree:
     
     def __init__(self) -> None:        
         self.root = None
         
-
-
     def add_node(self, node):
 
         if self.root is None:
@@ -28,8 +24,7 @@ class Fname_tree:
             if node.customer.id == temp.customer.id:
                 return
 
-            if node.customer.fname > temp.customer.fname:
-                
+            if node.customer.fname > temp.customer.fname:                
                 if not temp.right:
                     temp.right = node
                     return
@@ -43,13 +38,65 @@ class Fname_tree:
 
 
 
+class Lname_tree:
+    
+    def __init__(self) -> None:        
+        self.root = None
+        
+    def add_node(self, node):
+
+        if self.root is None:
+            self.root = node
+            return
+        
+        temp = self.root
+        while True:
+            if node.customer.lname > temp.customer.lname:
+                
+                if not temp.right:
+                    temp.right = node
+                    return
+                temp = temp.right
+            
+            else:                
+                if not temp.left:
+                    temp.left = node
+                    return
+                temp = temp.left
+
+
+class Debt_tre:
+    
+    def __init__(self) -> None:        
+        self.root = None
+
+    def add_node(self, node):
+
+        if self.root is None:
+            self.root = node
+            return
+        
+        temp = self.root
+        while True:
+            if node.customer.debt > temp.customer.debt:
+                
+                if not temp.right:
+                    temp.right = node
+                    return
+                temp = temp.right
+            
+            else:                
+                if not temp.left:
+                    temp.left = node
+                    return
+                temp = temp.left
+
+#  Binary tree sorted by ID
 class Id_tree:
     
     def __init__(self) -> None:        
         self.root = None
         
-
-
     def add_node(self, node):
         if self.root is None:
             self.root = node
@@ -57,8 +104,6 @@ class Id_tree:
         
         temp = self.root
         while True:
-            if node.customer.id == temp.customer.id:
-                return
 
             if node.customer.id > temp.customer.id:
                 
