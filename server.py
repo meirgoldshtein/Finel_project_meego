f44283c8b4d38ff1907366d4e649167fcaf1b598
meirgoldshtein <m0548491057@gmail.com>
2024-02-20 11:23:55 +0200

    A function to check the correctness of consumer details has been added

--- server.py
+++ server.py
@@ -47,7 +47,60 @@ for customer in customers_list:
 def print_query(filtered_list):
     for customer in filtered_list:
         print (f"name: {customer.fname} {customer.lname}, ID: {customer.id}, phone: {customer.phone}, debt: {customer.debt}, date: {customer.data}\n")
-     
+
+
+def is_israeli_id(id):
+    list1 = str(id).split()
+    list2 = [1,2,1,2,1,2,1,2,1]
+    sum = 0
+    i = 0
+    for i in range(len(list1)):
+        x = list1[i] * list2[i]
+        if x > 9:
+            y = str(x)
+            x = int(y)[0] + int(y)[1]
+            sum += x
+    if sum % 10 == 0:
+        return True
+    return False
+
+
+
+def customer_checking(customer: list):
+
+    correct = False
+    if len(customer) < 6:
+        print("One or more of the data is missing")
+    elif len(customer) > 6:
+        print("Error, too many details have been entered")
+    if not customer[0].isalpha() or not customer[1].isalpha():
+        print("The consumer name must be of type string")
+    if not customer[2].isdigit():
+        print("ID must be numbers only")
+    if len(customer[2]) != 9:
+        print("ID is not valid. Must contain 9 digits")
+    if not customer[3].isdigit():
+        print("Phone number must be numbers only")
+    if len(customer[3]) != 10:
+        print("Phon number is not valid. Must contain 10 digits")
+    try:
+        float(customer[4])
+    except ValueError:
+        print("Debt must be numbers only")
+    date = customer[5]
+    if not (date[1] == "/" or date[2] == "/") and (date[4] == "/" or date[5] == "/"):
+        print("The date structure is incorrect\nMust be in the format  dd/mm/yyyy")
+    date = date.split("/")
+    if not date[0].isdigit() or 0 <= int(date[0]) <= 31:
+        print("The day entered is not valid")
+    if not date[1].isdigit() or 0 <= int(date[1]) <= 12:
+        print("The month entered is not valid")  
+    if not date[2].isdigit() or 0 <= int(date[2]):
+        print("The month entered is not valid")      
+    
+    return correct
+
+
 while True:
     try:
         query = input(">>> ").split(" ")
@@ -76,12 +129,8 @@ while True:
             else:
                 print("No results")
 
-
-
-
     except ValueError:
         print("Invalid query please enter again")
 
         
-
 
